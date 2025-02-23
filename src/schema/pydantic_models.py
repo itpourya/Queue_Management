c89e6299f7a0892527c635c2c9b4ae804f5ab4fd
Pourya Samimi <neverdiegenius@gmail.com>
2025-02-24 00:36:15 +0330

    feat: Create pydantic model for better typing anotaion

---
+++ src/schema/pydantic_models.py
@@ -0,0 +1,18 @@
+from uuid import UUID
+from pydantic import BaseModel
+
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+
+
+class TokenData(BaseModel):
+    username: str
+    id: UUID
+    email: str
+
+class RegisterFields(BaseModel):
+    username: str
+    email: str
+    password: str
