2216a46519d8ef2a9be37d3006a0ce88103a14ce
Pourya Samimi <neverdiegenius@gmail.com>
2025-02-27 23:07:59 +0330

    feat: Create login service to check user password and create jwt payload

--- src/service/user_service.py
+++ src/service/user_service.py
@@ -1,9 +1,11 @@
 from src.repository.user_repository import UserRepository
 from sqlalchemy.ext.asyncio import AsyncSession
-from src.schema.pydantic_models import RegisterFields, GetUserByUsername
+from src.schema.pydantic_models import RegisterFields, GetUserByUsername, LoginFields, Token
 from src.pkg.jwt import Security
 from src.models.user_model import User
 from typing import Tuple
+from src.pkg.jwt import Security
+from fastapi import status, HTTPException
 
 class UserService:
     def __init__(self, session: AsyncSession):
@@ -24,3 +26,26 @@ class UserService:
         user = self.repo.get(user_input=user_data)
 
         return True, user[1]
+
+    async def login_user_service(self, user_input: LoginFields) -> Tuple[bool, Token]:
+        if not user_input.username or not user_input.password:
+            return False, None
+
+        user = await self.repo.get(username=user_input.username)
+        if not user[0]:
+            return False, None
+
+        if Security.verify_password(user_input.password, user[1].hashed_password):
+            token = Security.create_access_token({
+                "username": user[1].username,
+                "email": user[1].email
+            })
+
+            return True, Token(access_token=token, token_type="bearer")
+
+        else:
+            raise HTTPException(
+                status_code=status.HTTP_401_UNAUTHORIZED,
+                detail="Incorrect username or password",
+                headers={"WWW-Authenticate": "Bearer"},
+            )
