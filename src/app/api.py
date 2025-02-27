8db9d0e1d7e1fb5b9b555b31e372f838431ec103
Pourya Samimi <neverdiegenius@gmail.com>
2025-02-27 20:56:03 +0330

    feat: add lifespan for initialize database tables

--- src/app/api.py
+++ src/app/api.py
@@ -2,11 +2,12 @@ import sys, os
 sys.path.append(os.path.dirname(os.path.abspath(__file__)) + "/../..")
 from src.app.route import api_router, task_router
 from fastapi import APIRouter, FastAPI
+from src.settings.lifespan import init_db
 
 app_router: APIRouter = APIRouter()
 
 app_router.include_router(router=api_router, prefix="/api/v1/auth")
 app_router.include_router(router=task_router, prefix="/api/v1/tasks")
 
-app = FastAPI()
+app = FastAPI(lifespan=init_db)
 app.include_router(app_router)
